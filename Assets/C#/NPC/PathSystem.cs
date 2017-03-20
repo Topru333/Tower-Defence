21dba5705d1d4960eee94055e7ed75ce0fe7d26f
nikita shaposhnikov <topru333@hotmail.com>
2017-03-20 06:33:17 +0300

    blah

--- Assets/C#/NPC/PathSystem.cs
+++ Assets/C#/NPC/PathSystem.cs
@@ -3,18 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PathSystem : MonoBehaviour {
-    private System.Random rand = new System.Random();
+    public static PathSystem instance;
 
-    private List<Point> Points = new List<Point>();                                // Список имеющихся точек
-    private List<Edge> Edges = new List<Edge>();                                   // Список имеющихся ребер
+    public int Start_id;                                                            // Айди стартовой точки для спавна нпс
 
+
+    private float Radius = 2;                                                       // Радиус отрисованной точки
+
+    private static System.Random rand = new System.Random();
+
+    private static List<Point> Points = new List<Point>();                          // Список имеющихся точек
+    private static List<Edge> Edges = new List<Edge>();                             // Список имеющихся ребер
+
+    private float timer,delay;
+
+    /// <summary>
+    /// Функция вызова волны NPC
+    /// </summary>
+    /// <param name="wave">структура волны(все данный о волне в ней)</param>
+    /// <param name="id">Индекс вершины старта волны</param>
+    static void NPCSpawn (NpcWave wave,int id) {
+        //todo: Спавнит мобов в вершине...
+    }
+    void Awake () {
+        if (instance != null) { Debug.LogError("More than 1 Path system on the level"); return; }
+        instance = this;
+    }
+    void Update () {
+        timer += Time.deltaTime;
+    }
 
     /// <summary>
     /// Поиск следующей точки по айди
     /// </summary>
     /// <param name="id">айди точки на входе ребра</param>
     /// <returns>айди точки на выходе ребра</returns>
-    public Point PointSearch (int id) {
+    public static Point PointSearch (int id) {
         List<Edge> found = new List<Edge>();                                        //список ребер что были найдены
         int thisone = -1;
 
@@ -34,6 +58,25 @@ public class PathSystem : MonoBehaviour {
         return null;                                                                // если не найдена возвращаем нулл
     }
 
+    public void OnDrawGizmos () {
+        Gizmos.color = new Color(Color.cyan.r, Color.cyan.g, Color.cyan.b, 0.5f);
+        for (int i = 0; i < Points.Count; i++) {
+            Gizmos.DrawSphere(Points[i].Position, Radius);
+        }
+    }
+
+    public static void Init () {
+        /* Заполняем списки вершин и ребер в ручную (Points, Edges)+ unit test */
+        /* В векторе пишем X и Z, это примеры */
+        Points.Add(new Point(0, new Vector2(1, 8)));
+        Points.Add(new Point(1, new Vector2(4, 8)));
+
+        Edges.Add(new Edge(0,1));
+
+        Points.Add(new Point(2, new Vector2(4, 6)));
+
+        Edges.Add(new Edge(1, 2));
+    }
 }
 
                                                                                     //Струкутра для ребра с переменными:айди начала и айди выхода точки
@@ -79,3 +122,5 @@ public class Point {
     }
 }
 
+public struct NpcWave {
+}
