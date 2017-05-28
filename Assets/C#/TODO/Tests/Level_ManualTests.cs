be705ecb7d92670c38b10647a594041c7bcce9f2
petrgeorgievsky <petrgeorgievsky@gmail.com>
2017-05-28 13:21:12 +0300

    добавил недостающие файлы

---
+++ Assets/C#/TODO/Tests/Level_ManualTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+namespace TD.Tests
+{
+    public class Level_ManualTests : MonoBehaviour
+    {
+        public string name;
+        // Use this for initialization
+        void Awake()
+        {
+            LevelManager.Instance.LevelLoad(name);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
